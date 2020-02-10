cf380dd131e131b24d8006fbec1a47564b3de967
Lizzie Siegle <esiegle@brynmawr.edu>
2020-02-09 18:19:10 -0800

    final

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ contractions = {
     "haven't": "have not",
     "how'd": "how did",
     "how'll": "how will",
-    "how's": "how is",
+    "how's": "how has / how is",
     "i'd": "I had",
     "i'll": "I will",
     "i'm": "I am",
@@ -67,47 +67,51 @@ lemma = WordNetLemmatizer()
 def tokenize_and_stem_text(text): #stem each word in text
     return [lemma.lemmatize(word.lower()) for word in text] 
 
+# get a list of all categories to train for: loves_me, loves_me_not
 binary_categories = list(data.keys())
+training_words = []
+# a list of tuples with words in the sentence and category name
 json_data = []
-def read_training_data(data, training_words):
-    for each_category in data.keys(): 
-        for each_sentence in data[each_category]:
-            for word in each_sentence.split():
+def read_training_data(data): #modify training_words
+    for category in data.keys(): 
+        for text in data[category]:
+            for word in text.split():
                 if word.lower() in contractions:
-                    each_sentence = each_sentence.replace(word, contractions[word.lower()])
+                    text = text.replace(word, contractions[word.lower()])
             # remove  punctuation from the sentence
-            each_sentence = re.sub("[^a-zA-Z' ]+", ' ', each_sentence)
+            text = re.sub("[^a-zA-Z' ]+", ' ', text)
             # extract words from each sentence and append to the word list
-            training_words.extend(word_tokenize(each_sentence))
-            json_data.append((word_tokenize(each_sentence), each_category))
+            training_words.extend(word_tokenize(text))
+            json_data.append((word_tokenize(text), category))
     return json_data
 
-
 # stem and lower each word and remove duplicates
-training_words = tokenize_and_stem_text(training_words)
-read_training_data(data, training_words)
-
-
+tokenize_and_stem_text(training_words)
+read_training_data(data)
 # create training data
-training_data = []
+training = []
 for item in json_data:
     # initialize our bag of words(bow) for each document in the list
-    vector = []
+    bag_vector = []
     # list of tokenized words for the pattern
     token_words = item[0]
     # stem each word
     token_words = [lemma.lemmatize(word.lower()) for word in token_words]
+    print(f'{token_words}')
     # create our bag of words array
-    for word in training_words:
-        vector.append(1) if word in token_words else vector.append(0)
-    output_row = list([0] * len(binary_categories)) #empty array for our output
-    output_row[binary_categories.index(item[1])] = 1
+    for w in training_words:
+        if w in token_words:
+            bag_vector.append(1)
+        else:
+            bag_vector.append(0)
+    out_row = list([0] * len(binary_categories)) #empty array for our output
+    out_row[binary_categories.index(item[1])] = 1
     # our training set will contain a the bag of words model and the output row that tells
-    # which catefory that bow belongs to.
-    training_data.append([vector, output_row])
+    # which category that bow belongs to.
+    training.append([bag_vector, out_row])
     
 # shuffle features,  turn into np.array bc tf takes in numpy array
-random.shuffle(training)
+# random.shuffle(training)
 training = np.array(training)
 print(f'training {training}')
 # data contains the bag of words and labels contains the label/category
@@ -118,29 +122,25 @@ print(f'labels {labels}')
 # reset underlying graph data, clear defined variables and operations of the previous cell
 tf.reset_default_graph()
 # Build neural network: 3 layers. input_data layer is used for inputting (aka. feeding) data to a network. the input to your network has shape len(data[0])
+print(f'len(data[0] {len(data[0])}, {data[0]}')
 net = tflearn.input_data(shape=[None, len(data[0])]) #none = unknown dimension, so can change total # samples processed in batch
 net = tflearn.fully_connected(net, 32) #32 hidden units/neurons
-net = tflearn.fully_connected(net, 32)
 net = tflearn.fully_connected(net, len(labels[0]), activation='softmax') #softmax vs sigmoid: 
 net = tflearn.regression(net)
 # Define model and setup tensorboard: DNN automatically performs NN classifier tasks like training, prediction
 model = tflearn.DNN(net)
 # Start training (apply gradient descent algorithm). epochs = # times the network will see all data
-model.fit(data, labels, n_epoch=100, batch_size=16, show_metric=True)
-
-# let's test the model for a few sentences:
-# the first two sentences are used for training, and the last two sentences are not present in the training data.
-sent_1 = "i picked up pizza for you on the way home"
+model.fit(data, labels, n_epoch=1000, batch_size=16, show_metric=True)
 
 # method takes in a sentence + list of all words, returns data in form that  can be fed to tensorflow
 def clean_for_tf(text):
     input_words = tokenize_and_stem_text(word_tokenize(text))
-    vector = [0]*len(training_words)
+    bag_vector = [] #[0]*len(training_words)
     for input_word in input_words:
         for ind, word in enumerate(training_words):
             if word == input_word:
-                vector[ind] = 1
-    return(np.array(vector))
+                bag_vector[ind] = 1
+    return(np.array(bag_vector))
 
 app = Flask(__name__)
 @app.route("/sms", methods=['POST'])
